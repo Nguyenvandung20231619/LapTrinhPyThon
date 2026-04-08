c98b15ea85be59916bded1b28dba40eabf04a595
Nguyenvandung20231619 <nvd7125@gmail.com>
2026-04-08 21:34:48 +0700

    nộp bai tuple

---
+++ Nguyễn Văn Dũng(1619)/Tuple/bai1.py
@@ -0,0 +1,8 @@
+_tuple = ('a', 'b', 'd', 'e')
+
+phan_dau = _tuple[0:2] 
+phan_tu_moi = ('c',)
+phan_duoi = _tuple[2:]
+_new_tuple = phan_dau + phan_tu_moi + phan_duoi
+
+print("new tuple:", _new_tuple) 
